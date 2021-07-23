bd919d6682369c794105c02dea4045bd1db34226
Josevimo <87067360+Josevimo@users.noreply.github.com>
2021-07-23 10:26:58 +0200

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,8 +1,8 @@
 public class prueba{
     public void pruebajenkins(String s) {
         // Convertir a upercase
-        s="hola";
-        System.out.println ("hola");
+        
+        System.out.println (s.toUpperCase());
 
     }
 }
