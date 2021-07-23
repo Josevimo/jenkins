7b3b78e97ce9d92f0664400d485117e0550262a4
Josevimo <87067360+Josevimo@users.noreply.github.com>
2021-07-23 10:06:09 +0200

    Update Saludar..java

--- Saludar..java
+++ Saludar..java
@@ -1,1 +1,1 @@
-package inicio; public class Saludar { public static  void main(String args[]) { System.out.println("Este es mi primer jemplo en Java"); } }
+public class Saludar { public static  void main(String args[]) { System.out.println("Este es mi primer jemplo en Java"); } }
